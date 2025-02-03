5a236d2d2adfebd02d77a5d0d9e27908927a4ac8
Juan Vanegas <juan.vanegas@loka.com>
2025-02-03 11:47:50 -0500

    :sparkles: Unary addition calculator

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 
+#[derive(Clone, Copy, Debug)]
 enum Direction {
     Left,
     Right,
@@ -78,7 +79,7 @@ fn main() {
                 1,
                 StateOperation {
                     write: None,
-                    move_head: Some(Direction::Right),
+                    move_head: Some(Direction::Left),
                     next_state: Some(2),
                 },
             ),
@@ -95,4 +96,44 @@ fn main() {
             },
         )]),
     };
+
+    let mut tape = vec![0, 1, 1, 0, 1, 1, 1];
+    let mut head = 0;
+    let mut state = &q0;
+
+    loop {
+        println!("{:?}", tape);
+        println!("head at: {:?}", head);
+        println!("current state: {:?}", state.identifier);
+        let current_value = tape[head];
+        let operation = state.operations.get(&current_value).unwrap();
+        if let Some(write) = operation.write {
+            tape[head] = write;
+        }
+        if let Some(move_head) = operation.move_head {
+            match move_head {
+                Direction::Left => {
+                    head -= 1;
+                }
+                Direction::Right => {
+                    head += 1;
+                }
+            }
+        }
+        if let Some(next_state) = operation.next_state {
+            if next_state == -1 {
+                break;
+            }
+            match next_state {
+                0 => state = &q0,
+                1 => state = &q1,
+                2 => state = &q2,
+                3 => state = &q3,
+                -1 => state = &halt,
+                _ => state = &halt,
+            }
+        }
+    }
+    println!("\n{:?}", tape);
+    println!("halted");
 }
