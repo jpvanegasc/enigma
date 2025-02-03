5248e93421bbeb0f961cc4428d08e32599f345b9
Juan Vanegas <juan.vanegas@loka.com>
2025-02-03 11:26:25 -0500

    :sparkles: Basic state implementation

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,98 @@
-use std::env;
-use std::fs;
-use std::process;
+use std::collections::HashMap;
+
+enum Direction {
+    Left,
+    Right,
+}
+
+struct State {
+    identifier: i32,
+    operations: HashMap<i32, StateOperation>,
+}
+
+struct StateOperation {
+    write: Option<i32>,
+    move_head: Option<Direction>,
+    next_state: Option<i32>,
+}
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    if args.len() < 2 {
-        eprintln!("Usage: {} <filename>", args[0]);
-        process::exit(1);
-    }
-
-    let filename = &args[1];
-    match fs::read_to_string(filename) {
-        Ok(contents) => println!("{}", contents),
-        Err(e) => eprintln!("Error reading file {}: {}", filename, e),
-    }
+    let halt = State {
+        identifier: -1,
+        operations: HashMap::new(),
+    };
+    let q0 = State {
+        identifier: 0,
+        operations: HashMap::from([
+            (
+                0,
+                StateOperation {
+                    write: None,
+                    move_head: Some(Direction::Right),
+                    next_state: Some(0),
+                },
+            ),
+            (
+                1,
+                StateOperation {
+                    write: None,
+                    move_head: Some(Direction::Right),
+                    next_state: Some(1),
+                },
+            ),
+        ]),
+    };
+    let q1 = State {
+        identifier: 1,
+        operations: HashMap::from([
+            (
+                0,
+                StateOperation {
+                    write: Some(1),
+                    move_head: Some(Direction::Left),
+                    next_state: Some(2),
+                },
+            ),
+            (
+                1,
+                StateOperation {
+                    write: None,
+                    move_head: Some(Direction::Right),
+                    next_state: Some(1),
+                },
+            ),
+        ]),
+    };
+    let q2 = State {
+        identifier: 2,
+        operations: HashMap::from([
+            (
+                0,
+                StateOperation {
+                    write: None,
+                    move_head: Some(Direction::Right),
+                    next_state: Some(3),
+                },
+            ),
+            (
+                1,
+                StateOperation {
+                    write: None,
+                    move_head: Some(Direction::Right),
+                    next_state: Some(2),
+                },
+            ),
+        ]),
+    };
+    let q3 = State {
+        identifier: 3,
+        operations: HashMap::from([(
+            1,
+            StateOperation {
+                write: Some(0),
+                move_head: None,
+                next_state: Some(-1),
+            },
+        )]),
+    };
 }
